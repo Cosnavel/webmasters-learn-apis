13173fe7ba08b00de14ef74c7a39101f55bb75d8
Cosnavel <niclasdevelop@gmail.com>
2020-11-12 17:42:40 +0100

    added Film Model & Resource

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Resources\Film as FilmResource;
+use App\Models\Film;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +18,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/films', function () {
+    return FilmResource::collection(Film::all());
+});
+
+Route::get('/films/{id}', function ($film) {
+    return FilmResource::collection(Film::find($film));
 });
