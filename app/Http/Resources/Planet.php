3477451c7ddc0d91272884d002bbc3f7e762274e
Cosnavel <coding@niclaskahlmeier.de>
2021-10-31 23:35:30 +0100

    remove dead code

--- app/Http/Resources/Planet.php
+++ app/Http/Resources/Planet.php
@@ -6,14 +6,4 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class Planet extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array
-     */
-    public function toArray($request)
-    {
-        return parent::toArray($request);
-    }
 }
