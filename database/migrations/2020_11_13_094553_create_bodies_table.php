6259e8858c99109d4e3b6a0f372bcd70b67376fa
Cosnavel <coding@niclaskahlmeier.de>
2021-10-31 23:29:43 +0100

    anonymous migrations

--- database/migrations/2020_11_13_094553_create_bodies_table.php
+++ database/migrations/2020_11_13_094553_create_bodies_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBodiesTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -52,4 +52,4 @@ class CreateBodiesTable extends Migration
     {
         Schema::dropIfExists('bodies');
     }
-}
+};
